075c9647b7568624a26ee1e4b81dc849b738a200
Dien Nguyen <dienlk456@gmail.com>
2021-07-09 23:24:50 +0700

    delete all file in main

--- src/lib/commerce.js
+++
@@ -1,6 +0,0 @@
-import Commerce from "@chec/commerce.js";
-// khởi tạo object commerce ( ecommerce store)
-// truyền vào parameter là public key
-export const commerce = new Commerce(
-  "pk_test_27727a38246c5809bbd14ade78d688ae0e551e6ff3f9d"
-);
