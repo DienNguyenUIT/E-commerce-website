a6e8d93ae779da0f29bd6c8914adab8b5b62d11c
Dien Nguyen <dienlk456@gmail.com>
2021-07-10 09:27:50 +0700

    delete src, public

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Navbar } from "./Navbar/Navbar";
-export { default as Products } from "./Products/Products";
-export { default as Cart } from "./Cart/Cart";
-export { default as Checkout } from "./CheckoutForm/Checkout/Checkout";
-export { default as Detail } from "./Products/Detail/Detail";
-
-// import de co the render trong file App.js
